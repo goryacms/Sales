2cc6ee092be3d0c1a564886575dac5942b3eae01
maxim.goryachev <maxim.goryachev@corp.codeinside.ru>
2019-03-04 17:29:38 +0300

    Initial commit

---
+++ modules/global/src/com/company/sales/entity/Order.java
@@ -0,0 +1,58 @@
+package com.company.sales.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+@Table(name = "SALES_ORDER")
+@Entity(name = "sales$Order")
+public class Order extends StandardEntity {
+    private static final long serialVersionUID = -8702950762044452004L;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CUSTOMER_ID")
+    protected Customer customer;
+
+    @Temporal(TemporalType.DATE)
+    @NotNull
+    @Column(name = "DATE_", nullable = false)
+    protected Date date;
+
+    @Column(name = "AMOUNT")
+    protected BigDecimal amount;
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+
+}
